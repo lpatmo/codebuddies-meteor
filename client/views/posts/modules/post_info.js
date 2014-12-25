6f4ec10c4d367b54e8427bff29451192db0c0115
lpatmo <linda.peng@alumni.duke.edu>
2014-12-25 14:56:48 -0500

    changes to terminology and home page

--- client/views/posts/modules/post_info.js
+++ client/views/posts/modules/post_info.js
@@ -1,6 +1,6 @@
 Template[getTemplate('postInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('point') : i18n.t('points');
+    return this.upvotes == 1 ? i18n.t('member') : i18n.t('members');
   },
   can_edit: function(){
     return canEdit(Meteor.user(), this);
