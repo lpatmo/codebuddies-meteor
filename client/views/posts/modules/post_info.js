c80d6f2ee7a97bac3393d26f2c4cb0d4ac674629
lpatmo <linda.peng@alumni.duke.edu>
2014-12-29 01:01:09 -0500

    changed members to participants - text tweak

--- client/views/posts/modules/post_info.js
+++ client/views/posts/modules/post_info.js
@@ -1,6 +1,6 @@
 Template[getTemplate('postInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('member') : i18n.t('members');
+    return this.upvotes == 1 ? i18n.t('participant') : i18n.t('participants');
   },
   can_edit: function(){
     return canEdit(Meteor.user(), this);
